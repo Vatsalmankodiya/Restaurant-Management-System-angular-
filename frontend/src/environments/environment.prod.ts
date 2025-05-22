35a00b005ab6831c96af8ed10a5ea96e3a72e1cb
Vatsal Mankodiya <101264005+Vatsalmankodiya@users.noreply.github.com>
2025-05-22 12:37:08 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://restaurant-management-system-angular-2.onrender.com'
+  apiUrl: 'https://restaurant-management-system-angular-1.onrender.com'
 };
